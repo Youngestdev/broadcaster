5394f312efd91f0a639f1f84e7a5df13d8ce2bab
Abdulazeez Abdulazeez Adeshina <youngestdev@gmail.com>
2025-04-21 22:09:22 +0100

    Renamed the folder so change effected.

--- mongo_broadcaster/utils.py
+++ mongo_broadcaster/utils.py
@@ -1,7 +1,7 @@
 import logging
 from typing import Any, Dict
 
-from broadcaster.models import ChangeEvent, CollectionConfig
+from mongo_broadcaster.models import ChangeEvent, CollectionConfig
 
 logger = logging.getLogger(__name__)
 
